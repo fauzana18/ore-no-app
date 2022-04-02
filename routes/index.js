1b8cd1264f7c672f37e49bcaa8d55d8130791a94
fauzana18 <fauzana18@gmail.com>
2022-04-02 17:19:43 +0700

    tes env

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ore no App' });
 });
 router.get('/tes', function(req, res, next) {
-  res.send('bisa nih!');
+  res.send(process.env.TES);
 });
 
 module.exports = router;
