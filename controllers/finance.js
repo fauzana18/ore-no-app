907a588cbc7b5c3539dea103a7cd1ac666faf800
fauzana18 <fauzana18@gmail.com>
2022-04-09 13:34:03 +0700

    no message

--- controllers/finance.js
+++ controllers/finance.js
@@ -51,7 +51,9 @@ module.exports = {
                     saldo.categorized[element.category.name.replace(' ', '_')] = {pengeluaran: 0, pemasukan: 0}
                 }
                 saldo.categorized[element.category.name.replace(' ', '_')][element.category.type.toLowerCase()] += element.amount
-            });
+            })
+            const date = new Date(dbRes[0].created).getTimezoneOffset()
+            saldo.tz = date
 
             code = 200
             result = saldo
